3b7979eb06f9a1538925b03e572d1538ff476d46
Darby Johnson <darbyjjohnson1@gmail.com>
2021-12-09 13:46:35 -0500

    started to try and get the data to populate when searching for a location but decided to work on something else and will come back

--- assetss/script.js
+++ assetss/script.js
@@ -1,3 +1,15 @@
+var searchInput = document.getElementById("searchForm.searchInput");
+const searchBtn = document.getElementById("searchBtn");
+// const mainContent = $("#mainContent");
+// var recentSearches = $("#recents");
+// const current = $("#current");
+// const fiveDay = $("#five-day");
+// const clearBtn = $("#clearBtn");
+
+const weather = data;
+weather.temperature = {
+    unit : "fahrenheit"
+}
 // function setPosition(position){
 //     let latitude = position.coords.latitude;
 //     let longitude = position.coords.longitude;
@@ -5,13 +17,13 @@
 //     getWeather(latitude, longitude);
 
 // function getWeather(latitude, longitude){
-//     let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1a569bb7d37723f9bf3b10e34026f005&units=imperial`
+//     let api = `http://api.openweathermap.org/data/2.5/weather?&appid=1a569bb7d37723f9bf3b10e34026f005&units=imperial`
 // }      
-
-document.getElementById("search-button").addEventListener("click", function(){
+// lat=${latitude}&lon=${longitude}
+document.getElementById("searchBtn").addEventListener("click", function(){
     var searchInput = document.getElementById("searchInput")
 console.log(searchInput)
-    fetch(api)
+    fetch("http://api.openweathermap.org/data/2.5/weather?q="+searchInput+"&appid=1a569bb7d37723f9bf3b10e34026f005&units=imperial")
     .then(response => {
         let data = response.json();
        return data;
